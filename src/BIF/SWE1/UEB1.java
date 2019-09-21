8fca4636a7b18bdd7b4f3d09036b306d7462acb6
Philip Ewert <philipewert@icloud.com>
2019-09-21 15:46:19 +0200

    Project Setup
    Unit Tests
    Interfaces
    Templates

---
+++ src/BIF/SWE1/UEB1.java
@@ -0,0 +1,14 @@
+package BIF.SWE1;
+
+import BIF.SWE1.interfaces.Url;
+
+public class UEB1 {
+
+	public Url getUrl(String path) {
+		//TODO: Implement URL Interface
+		return null;
+	}
+
+	public void helloWorld() {
+	}
+}
--- src/com/philipewert/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-package com.philipewert;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-    }
-}
